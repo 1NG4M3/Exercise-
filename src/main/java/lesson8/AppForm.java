14ef5aed5eaed14843489c4dd27cc8b9acb0fd2b
1NG4M3 <DimaGyss@mail.ru>
2023-01-02 20:43:15 +0300

    Написание приложения с простым графическим интерфейсом

---
+++ src/main/java/lesson8/AppForm.java
@@ -0,0 +1,27 @@
+package lesson8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AppForm extends JFrame {
+
+    public AppForm() {
+        // Создаем окно:
+        setTitle("Test Window");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(500, 200, 400, 400);
+
+        // Добавляем две кнопки:
+        JButton northButton = new JButton("Button #1 Noth");
+        add(northButton, BorderLayout.NORTH);
+        JButton southButton = new JButton("Button #2 South");
+        add(southButton, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AppForm();
+    }
+}
+
---
+++ src/main/java/lesson8/TestWindow2.java
@@ -0,0 +1,26 @@
+package lesson8;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+
+public class TestWindow2 extends JFrame{
+    public TestWindow2() {
+        setBounds(300, 300, 720, 300);
+        setTitle("BoxLayoutDemo");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+        for (int i = 6; i > 0; i--) {
+            add(new JButton("Вагон № " + i + " ]"));
+        }
+        add(new JButton("Локомотив"));
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TestWindow2();
+    }
+}
